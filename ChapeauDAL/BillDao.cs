bd7a352f66a69af1c37bc2feaa8f104bbfba210a
NaomideJong <562748@student.inholland.nl>
2022-05-27 15:39:30 +0200

    most recent 27/5

--- ChapeauDAL/BillDao.cs
+++ ChapeauDAL/BillDao.cs
@@ -17,18 +17,17 @@ namespace ChapeauDAL
         public void AddBill(Bill bill)
         {
             string query = "INSERT INTO [Bill] VALUES (@bill_Id, @table_Id, @staff_ID, @totalPriceInclVAT, @totalPriceExclVAT, @totalVAT, @tip, @isPaid, @discount, @currentDate, @comments);";
-            SqlParameter[] sqlParameters = new SqlParameter[11];
+            SqlParameter[] sqlParameters = new SqlParameter[10];
             sqlParameters[0] = new SqlParameter("@bill_Id", bill.BillID);
             sqlParameters[1] = new SqlParameter("@table_Id", bill.TableID);
             sqlParameters[2] = new SqlParameter("@staff_ID", bill.StaffID);
             sqlParameters[3] = new SqlParameter("@totalPriceInclVAT", bill.TotalPriceInclVAT);
             sqlParameters[4] = new SqlParameter("@totalPriceExclVAT", bill.TotalPriceExclVAT);
-            sqlParameters[5] = new SqlParameter("@totalVAT", bill.TotalVAT);
-            sqlParameters[6] = new SqlParameter("@tip", bill.Tip);
-            sqlParameters[7] = new SqlParameter("@isPaid", bill.IsPaid);
-            sqlParameters[8] = new SqlParameter("@discount", bill.Discount);
-            sqlParameters[9] = new SqlParameter("@currentDate", bill.Date);
-            sqlParameters[10] = new SqlParameter("@comments", bill.Comments);
+            sqlParameters[5] = new SqlParameter("@tip", bill.Tip);
+            sqlParameters[6] = new SqlParameter("@isPaid", bill.IsPaid);
+            sqlParameters[7] = new SqlParameter("@discount", bill.Discount);
+            sqlParameters[8] = new SqlParameter("@currentDate", bill.Date);
+            sqlParameters[9] = new SqlParameter("@comments", bill.Comments);
             ExecuteEditQuery(query, sqlParameters);
         }
         public List<Bill> ReadBillTables(DataTable dataTable)
