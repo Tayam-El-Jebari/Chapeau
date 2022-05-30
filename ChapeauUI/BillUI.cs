2798345766f227db6f761902a32d3cbffa822dfb
NaomideJong <562748@student.inholland.nl>
2022-05-30 21:29:57 +0200

    Cabin font added

--- ChapeauUI/BillUI.cs
+++ ChapeauUI/BillUI.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,16 @@ namespace ChapeauUI
         public BillUI()
         {
             InitializeComponent();
+        }
+        public void InitFont(Label label)
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            int fontLength = Properties.Resources.Cabin.Length;
+            byte[] fontdata = Properties.Resources.Cabin;
+            IntPtr data = Marshal.AllocCoTaskMem(fontLength);
+            Marshal.Copy(fontdata, 0, data, fontLength);
+            pfc.AddMemoryFont(data, fontLength);
+            label.Font = new Font(pfc.Families[0], label.Font.Size);
         }
     }
 }
--- ChapeauUI/Properties/Resources.Designer.cs
+++ ChapeauUI/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ChapeauUI.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -67,6 +67,16 @@ namespace ChapeauUI.Properties {
             get {
                 object obj = ResourceManager.GetObject("achtergrond", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Cabin {
+            get {
+                object obj = ResourceManager.GetObject("Cabin", resourceCulture);
+                return ((byte[])(obj));
             }
         }
         
